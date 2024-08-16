09d96e2f74332725977b3bfa6826609089d440cf
Giovanni Almirante <giovanni.almirante@exprivia.com>
2024-08-16 11:41:22 +0200

    updated to react

---
+++ precache-manifest.47aa451691d4e1b17ba1824094084ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0820c4f18f4bbe0dbea2ce47cd9c1320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7e75fdb70a18bffbd0",
+    "url": "/static/css/main.bd754a3f.chunk.css"
+  },
+  {
+    "revision": "bb38a582adb1376cf8fa",
+    "url": "/static/js/2.b703dd63.chunk.js"
+  },
+  {
+    "revision": "9a7e75fdb70a18bffbd0",
+    "url": "/static/js/main.a62a722e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ecd86cfb9c521ca5be47a49f5556b2f4",
+    "url": "/static/media/avatar.ecd86cfb.png"
+  }
+]);
